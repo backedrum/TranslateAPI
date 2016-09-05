9730c08e502af19257655cb56d0c93e713c66c8c
backedrum <andrey.zablodskiy@gmail.com>
2016-09-06 00:55:26 +0200

    Add: Initial version implementation.

---
+++ src/github.com/backedrum/server/dictionary/dictionary.go
@@ -0,0 +1,70 @@
+package dictionary
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+type Entry struct {
+	Original   string `xml:"form>orth"`
+	Translated string `xml:"sense>cit>quote"`
+}
+
+var dictMap = make(map[string]string)
+var reg, error = regexp.Compile("[^a-zA-Z\\d\\s:]+")
+
+func InitDictionary(path string) {
+	filePath, error := filepath.Abs(path)
+	if error != nil {
+		fmt.Println(error)
+		os.Exit(1)
+	}
+
+	xmlFile, error := os.Open(filePath)
+	if error != nil {
+		fmt.Println(error)
+		os.Exit(1)
+	}
+	defer xmlFile.Close()
+
+	decoder := xml.NewDecoder(xmlFile)
+
+	for {
+		token, _ := decoder.Token()
+		if token == nil {
+			break
+		}
+
+		switch startElement := token.(type) {
+		case xml.StartElement:
+			if startElement.Name.Local == "entry" {
+				var e Entry
+				decoder.DecodeElement(&e, &startElement)
+				// put pair into the map
+				dictMap[strings.ToLower(e.Original)] = e.Translated
+			}
+		}
+	}
+}
+
+func TranslateTextAsWordsList(text string) string {
+	words := strings.Split(reg.ReplaceAllString(text, ""), " ")
+
+	var buf bytes.Buffer
+
+	for i := range words {
+
+		if val, ok := dictMap[strings.ToLower(words[i])]; ok {
+			buf.WriteString(val + " ")
+		} else {
+			buf.WriteString(words[i] + " ")
+		}
+	}
+
+	return buf.String()
+}
---
+++ src/github.com/backedrum/server/server.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/xml"
+	"github.com/backedrum/server/dictionary"
+	"net/http"
+)
+
+type ServerResponse struct {
+	Original        string
+	Translated      string
+	OriginalLang    string
+	TranslateToLang string
+}
+
+func translate(res http.ResponseWriter, req *http.Request) {
+	text := req.URL.Query().Get("text")
+	from := req.URL.Query().Get("from")
+	to := req.URL.Query().Get("to")
+
+	response := ServerResponse{text, dictionary.TranslateTextAsWordsList(text), from, to}
+
+	xml, error := xml.MarshalIndent(response, "", "  ")
+	if error != nil {
+		http.Error(res, error.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res.Header().Set(
+		"Content-Type",
+		"application/xml",
+	)
+	res.Write(xml)
+}
+
+func main() {
+	// TODO get rid of hardcoded path
+	dictionary.InitDictionary("./src/github.com/backedrum/server/dictionary-files/nld-eng.tei")
+	http.HandleFunc("/translate", translate)
+	http.ListenAndServe(":9000", nil)
+}
